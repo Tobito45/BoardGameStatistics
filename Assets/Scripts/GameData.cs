9f45f79183cda0fa138bb0e2e94cdccfb7ac4e34
Tobito45 <borya.od@gmail.com>
2024-07-27 21:51:08 +0200

    Added loading images with url Fixed problem with elemnts size in scrollview

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UIElements;
+
+public class GameData
+{
+    private int id;
+    private string name;
+    private float mark;
+    private int countGames;
+    private string url;
+
+
+    public GameData(int id, string name, float mark, int countGames, string url)
+    {
+        this.id = id;
+        this.name = name;
+        this.mark = mark;
+        this.countGames = countGames;
+        this.url = url;
+    }
+
+    public void Installization(VisualElement root)
+    {
+        root.Q<Label>("Name").text = name;
+        root.Q<Label>("MarkText").text = mark.ToString("F1");
+        root.Q<Label>("GamesText").text = countGames.ToString();
+        LoadImageAsync(root.Q<VisualElement>("Image"));
+    }
+
+    private async void LoadImageAsync(VisualElement image)
+    {
+        Texture2D texture = await GetTextureFromUrlAsync(url);
+        if (texture != null)
+            image.style.backgroundImage = new StyleBackground(texture);
+    }
+
+    private async Task<Texture2D> GetTextureFromUrlAsync(string url)
+    {
+        using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(url))
+        {
+            var asyncOperation = www.SendWebRequest();
+
+            while (!asyncOperation.isDone)
+                await Task.Yield();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(www.error);
+                return null;
+            }
+            else
+                return DownloadHandlerTexture.GetContent(www);
+        }
+    }
+
+    /*WWW www = new WWW(url);
+    yield return www;
+    image.style.backgroundImage = new StyleBackground(www.texture);
+    */
+}
--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -9,50 +9,29 @@ public class MainController : MonoBehaviour
     private ScrollView _listView;
 
 
-    private List<string> _items;
+    private List<GameData> _items = new List<GameData>();
     [SerializeField]
     private VisualTreeAsset _prefabElement;
 
-    void Start()
+    private void Awake()
     {
-        _items = new List<string>() { "1", "2", "3", "4" };
-        _root = GetComponent<UIDocument>().rootVisualElement;
-        _listView = _root.Q<ScrollView>("List");
-        //_prefabElement = _root.Q<VisualElement>("TestElement");
-
-        //_listView.Add(_prefabElement);
-        //_prefabElement.style.display = DisplayStyle.Flex;
-
-        // Set up a make item function for a list entry
-        /*_listView.makeItem = () =>
-        {
-            // Instantiate the UXML template for the entry
-            var newListEntry = _prefabElement.Instantiate();
-
-            // Return the root of the instantiated visual tree
-            return newListEntry;
-        };
-
-        // Set up bind function for a specific list entry
-        _listView.bindItem = (item, index) =>
-        {
-        };
-
-        // Set a fixed item height matching the height of the item provided in makeItem. 
-        // For dynamic height, see the virtualizationMethod property.
-       // _listView.fixedItemHeight = 750;
-
-        // Set the actual item's source list/array
-        _listView.itemsSource = _items;
-    */
-
-        for (int i = 0; i < 5; i++)
-        {
-            var itemUi = _prefabElement.Instantiate();
-            _listView.Add(itemUi);
-
-        }
+        _items.Add(new GameData(0, "Dead by ", 7.2f, 10, "https://cf.geekdo-images.com/fpnjJSQWpAh1WZW3zJ807A__itemrep/img/0FyP7gUgtFxQS8vq2p_VocShE84=/fit-in/246x300/filters:strip_icc()/pic6727475.jpg"));
+        _items.Add(new GameData(1, "Spirit Island", 6.25f, 115, "https://cf.geekdo-images.com/kjCm4ZvPjIZxS-mYgSPy1g__itemrep/img/7AXozbOIxk5MDpn_RNlat4omAcc=/fit-in/246x300/filters:strip_icc()/pic7013651.jpg"));
+        _items.Add(new GameData(2, "Colt express", 6f, 2, "https://cf.geekdo-images.com/2HKX0QANk_DY7CIVK5O5fQ__itemrep/img/TvYm-n4tYlxtLfE2iU-aBDeRC5I=/fit-in/246x300/filters:strip_icc()/pic2869710.jpg"));
+        _items.Add(new GameData(2, "Colt express Colt expres", 6f, 2, "https://cf.geekdo-images.com/2HKX0QANk_DY7CIVK5O5fQ__itemrep/img/TvYm-n4tYlxtLfE2iU-aBDeRC5I=/fit-in/246x300/filters:strip_icc()/pic2869710.jpg"));
     }
 
-   
+
+    void Start()
+    {
+        _root = GetComponent<UIDocument>().rootVisualElement;
+        _listView = _root.Q<ScrollView>("List");
+
+        _items.ForEach(item =>
+        {
+            VisualElement itemUi = _prefabElement.Instantiate();
+            _listView.Add(itemUi);
+            item.Installization(itemUi);
+        });
+    }
 }
